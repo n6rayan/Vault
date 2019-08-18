0e8555d2a0b36c0915bba1e261ca044b868e5b3c
Nour Rayan <nour@broadbean.com>
2019-08-18 18:13:36 +0100

    Added React front-end client

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server started: http://localhost:${port}/`));
