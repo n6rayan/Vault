d9901983a6d0b400ac8badf500c634f0af6011f7
Nour Rayan <nour@broadbean.com>
2019-09-10 17:38:28 +0100

    Adding login routes to API

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use(require('./healthcheck'));
+router.use(require('./login'));
 router.use(require('./user'));
 
 module.exports = router;
---
+++ server/src/routes/login.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const router = require('express').Router();
+
+const log = require('../logger');
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      log.error(err);
+      return next(err);
+    }
+
+    if (!user) {
+      log.info(info);
+
+      info.success = 0;
+      return res.status(401).send(info);
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        log.error(err);
+        return next(err);
+      }
+
+      return res.send({
+        success: 1,
+        message: 'Successfully logged in!',
+        user: req.user
+      });
+    });
+  })(req, res, next);
+});
+
+router.get('/current-user', (req, res) => {
+  if (req.user) {
+    res.send({ user: req.user });
+  }
+  else {
+    res.send({ user: null });
+  }
+});
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.send(null);
+});
+
+module.exports = router;
