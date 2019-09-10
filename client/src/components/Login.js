20d68f38d7f23ae9c3045c8e217595e5b73fc783
Nour Rayan <nour@broadbean.com>
2019-09-10 17:34:42 +0100

    Adding login form and updating logic/functionality

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,22 +1,89 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
+
+import axios from 'axios';
+import { Button, Jumbotron, Form } from 'react-bootstrap';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      redirectPath: null
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ [name]: value });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify(this.state),
+      url: 'http://localhost:3001/login'
+    };
+
+    const result = await axios(options);
+    console.log(result);
+
+    this.form.reset();
+
+    if (result.data.success) {
+      this.setState({ redirectPath: '/' });
+    }
+    else {
+      this.setState({ redirectPath: '/login' });
+    }
+  }
 
   render = () => {
-    return (
-      <div className="CenterPage" align="center">
-        <form method="post" action="index.html">
-          <div class="box">
-            <h1>Login</h1>
+    if (this.state.redirectPath) {
+      return (<Redirect to={{ pathname: this.state.redirectPath }} />);
+    }
+    else {
+      return (
+        <Jumbotron>
+          <div>
+            <h1 style={{fontSize: '50px'}}>Login...</h1><br />
 
-            <input type="email" name="email" value="email" class="email" />
-            <input type="password" name="email" value="email" class="email" />
+            <Form onSubmit={this.handleSubmit} ref={form => this.form = form}>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control name="username" type="text" placeholder="Enter username..." onChange={this.handleChange} />
+              </Form.Group>
 
-            <a href="/">Login</a>
-            <a href="/">Cancel</a>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" placeholder="Enter password..." onChange={this.handleChange} />
+              </Form.Group>
+
+              <div>
+                <div style={{float: 'left'}}>
+                  <Button variant="primary" type="submit">
+                    Let's go!
+                  </Button>
+                </div>
+
+                <div style={{float: 'right'}}>
+                  <Button variant="primary" href="/">
+                    Cancel
+                  </Button>
+                </div>
+              </div>
+            </Form>
           </div>
-        </form>
-      </div>
-    );
+        </Jumbotron>
+      );
+    }
   }
 }
