48ce322e610c65b3c5164a0c5be8714b3a784cf4
Nour Rayan <nour@broadbean.com>
2019-09-10 17:36:20 +0100

    Adding sessions and setting up passport.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,39 @@
 const bodyParser = require('body-parser');
+const config = require('config');
 const cors = require('cors');
 const express = require('express');
+const session = require('express-session')
 const helmet = require('helmet');
 const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const FileStore = require('session-file-store')(session);
 const uuidv1 = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
 
+const helpers = require('./helpers');
 const log = require('./logger');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(helmet());
+
+app.use(session({
+  genid: () => {
+    return uuidv4(); // use UUIDs for session IDs
+  },
+  resave: false,
+  saveUninitialized: true,
+  secret: config.get('session.secret'),
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 morgan.token('reqid', req => req.reqid);
 
@@ -29,7 +50,39 @@ app.use(
       ":status :res[content-length] :response-time ':reqid'",
     { stream: log.stream }
   )
-)
+);
+
+passport.use(new LocalStrategy(async (username, password, done) => {
+  let user;
+
+  try {
+    user = await helpers.findUser(username);
+
+    const message = 'Incorrect Credentials!';
+
+    if (!user) {
+      return done(null, false, {message: message});
+    }
+
+    if (password !== user.password) {
+      return done(null, false, {message: message})
+    }
+  }
+  catch (err) {
+    return done(err);
+  };
+
+  return done(null, user);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await helpers.findUserById(id);
+  done(null, user);
+});
 
 app.use(routes);
 
