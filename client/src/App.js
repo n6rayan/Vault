173a0132e79580836b7e5461aa24707bdad93f21
Nour Rayan <nour@broadbean.com>
2019-09-11 16:36:53 +0100

    Updates to make sessions work with backend

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,15 @@ import { Button, Jumbotron } from 'react-bootstrap';
 import './assets/Vault.css';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedIn: null
+    };
+
+    this.userSessionExists = this.userSessionExists.bind(this);
+  }
 
   async componentDidMount() {
     await this.userSessionExists();
@@ -13,35 +22,53 @@ export default class App extends Component {
   async userSessionExists() {
     const options = {
       method: 'GET',
-      url: 'http://localhost:3001/current-user'
+      url: 'http://localhost:3001/current-user',
+      withCredentials: true
     };
 
     const response = await axios(options);
     console.log(response);
 
     if (response.data.user) {
-      this.setState({ loggedIn: true });
+      this.setState({
+        loggedIn: 1,
+        username: response.data.user.username
+      });
     }
     else {
-      this.setState({ loggedIn: false });
+      this.setState({
+        loggedIn: 0,
+        username: null
+      });
     }
   }
 
   render() {
-    return (
-      <Jumbotron>
-        <div align="right">
-          <p>Already registered? <a href="/login">Login</a></p>
+    if (this.state.loggedIn) {
+      return (
+        <div>
+          <p>Hi {this.state.username}</p>
+
+          <Button variant="primary">Logout</Button>
         </div>
+      );
+    }
+    else {
+      return (
+        <Jumbotron>
+          <div align="right">
+            <p>Already registered? <a href="/login">Login</a></p>
+          </div>
 
-        <div className="CenterPage" align="center">
-          <h1>Vault</h1><br />
+          <div className="CenterPage" align="center">
+            <h1>Vault</h1><br />
 
-          <p>Welcome to Vault, your centralised banking application!</p><br />
+            <p>Welcome to Vault, your centralised banking application!</p><br />
 
-          <Button variant="primary" href="/signup">Get Started</Button>
-        </div>
-      </Jumbotron>
-    );
+            <Button variant="primary" href="/signup">Get Started</Button>
+          </div>
+        </Jumbotron>
+      );
+    }
   }
 }
