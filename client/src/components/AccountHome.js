0c7e334edb76f6ab1548ee5c387a2052e9979b61
Nour Rayan <nour@broadbean.com>
2019-09-12 14:55:55 +0100

    Adding an AccountHome component

---
+++ client/src/components/AccountHome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import TopNav from './Navigation/TopNav';
+
+export default class AccountHome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <TopNav updateUser={this.props.updateUser} username={this.props.username} />
+    );
+  }
+}
