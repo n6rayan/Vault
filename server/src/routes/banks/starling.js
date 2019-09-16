c2026c8611098008dd7eb0291857c0d718690d4d
Nour Rayan <nour@broadbean.com>
2019-09-16 14:38:44 +0100

    Adding starling oauth routes to server

---
+++ server/src/routes/banks/starling.js
@@ -0,0 +1,39 @@
+const config = require('config');
+const router = require('express').Router();
+const uuid = require('uuid/v1');
+
+const starling = require('../../banks/starlingAPI');
+
+router.get('/oauth/login', (req, res) => {
+  const ouathState = uuid();
+  req.session.ouathState = ouathState;
+
+  const starlingConfig = config.get('starling');
+  const clientId = starlingConfig.clientId;
+  const redirectUrl = starlingConfig.redirectUrl;
+
+  const starlingOauthUrl = starlingConfig.oauthUrl;
+
+  const url = `${starlingOauthUrl}client_id=${clientId}&response_type=code&state=${ouathState}&redirect_uri=${redirectUrl}`;
+
+  res.redirect(url);
+});
+
+router.get('/oauth/redirect', async (req, res) => {
+  // const state = req.query.state;
+  const code = req.query.code;
+
+  // TODO: FIX ME - Session no persisting
+  // if (state && state !== req.session.ouathState) {
+  //   console.log(req.session.ouathState);
+  //   console.log('State did not match original state');
+  //   return res.status(400).send();
+  // }
+
+  const accessToken = await starling.getAccessToken(code);
+  req.session.accessToken = accessToken;
+
+  res.redirect('http://localhost:3000/');
+});
+
+module.exports = router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use(require('./healthcheck'));
 router.use(require('./login'));
 router.use(require('./user'));
+router.use('/starling', require('./banks/starling'));
 
 module.exports = router;
