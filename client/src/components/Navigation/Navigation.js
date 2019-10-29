b15d4ec0fbdd7526c63999f3ad31a1122af7f80b
Nour Rayan <nour@broadbean.com>
2019-10-29 18:08:55 +0000

    Minor visual updates to navbar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -33,7 +33,7 @@ export default class Navigation extends PureComponent {
 
   render() {
     return (
-      <Navbar bg="light" expand="lg" sticky="top">
+      <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
         <Navbar.Brand href="/account">Vault</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -41,13 +41,13 @@ export default class Navigation extends PureComponent {
             <Nav.Link href="/account/transactions">Transactions</Nav.Link>
           </Nav>
           <Form inline>
-            <Navbar.Text>Hello, {this.props.username}</Navbar.Text>
             <NavDropdown title="My Account" id="collasible-nav-dropdown">
               <NavDropdown.Item href="/account">Dashboard</NavDropdown.Item>
               <NavDropdown.Item href="/account/register-bank">Add Bank</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={this.logout}>Logout</NavDropdown.Item>
             </NavDropdown>
+            <Navbar.Text>Hello, {this.props.username}</Navbar.Text>
           </Form>
         </Navbar.Collapse>
       </Navbar>
