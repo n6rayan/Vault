21e955d769b1e4b215e136264ea659d3521ec05a
Nour Rayan <nour@broadbean.com>
2019-11-08 08:35:25 +0000

    Moving signup form HTML to own component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from "react-router-dom";
 
 import axios from 'axios';
-import { Button, Form } from 'react-bootstrap';
+
+import SignUpForm from './Forms/SignUpForm';
 
 export default class SignUp extends Component {
 
@@ -30,7 +31,7 @@ export default class SignUp extends Component {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       data: JSON.stringify(this.state),
-      url: 'http://localhost:3001/api/user'
+      url: 'http://vault.io:3001/api/user'
     };
 
     let result = await axios(options);
@@ -55,46 +56,7 @@ export default class SignUp extends Component {
       <div>
         <h1 style={{ fontSize: '50px' }}>Sign up...</h1><br />
 
-        <Form onSubmit={this.handleSubmit} ref={form => this.form = form}>
-          <Form.Group>
-            <Form.Label>Name</Form.Label>
-            <Form.Control name="name" type="text" placeholder="Who are you, buddy?" onChange={this.handleChange} />
-          </Form.Group>
-
-          <Form.Group>
-            <Form.Label>Username</Form.Label>
-            <Form.Control name="username" type="text" placeholder="Enter a username you wish to use!" onChange={this.handleChange} />
-          </Form.Group>
-
-          <Form.Group>
-            <Form.Label>Mobile Number</Form.Label>
-            <Form.Control name="phone" type="tel" placeholder="What number can we contact you at?" onChange={this.handleChange} />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control name="email" type="email" placeholder="What's your email?" onChange={this.handleChange} />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control name="password" type="password" placeholder="Shhh, keep this a secret!" onChange={this.handleChange} />
-          </Form.Group>
-
-          <div>
-            <div style={{ float: 'left' }}>
-              <Button variant="primary" type="submit">
-                Let&apos;s go!
-                </Button>
-            </div>
-
-            <div style={{ float: 'right' }}>
-              <Button variant="primary" href="/">
-                Cancel
-                </Button>
-            </div>
-          </div>
-        </Form>
+        <SignUpForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
       </div>
     );
   }
