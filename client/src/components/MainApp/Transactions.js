93fc615a1d23cdcf0570bbce48060f19e6a4c229
Nour Rayan <nour@broadbean.com>
2019-11-08 08:38:32 +0000

    Adding some test code to retrieve accounts list and update state

--- client/src/components/MainApp/Transactions.js
+++ client/src/components/MainApp/Transactions.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 export default class Transactions extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  async componentDidMount() {
+    const accounts = await axios({
+      method: 'GET',
+      url: 'http://vault.io:3001/api/starling/accounts',
+      withCredentials: true
+    });
+
+    this.setState(accounts);
+  }
+
   render() {
+    console.log(this.state);
     return <h1>Transactions</h1>;
   }
 }
