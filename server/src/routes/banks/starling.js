b64c9fa41251fd53086c09991a43fd5062d69de5
Nour Rayan <nour@broadbean.com>
2019-11-08 08:33:00 +0000

    Adding session error handling and additional route getting accounts

--- server/src/routes/banks/starling.js
+++ server/src/routes/banks/starling.js
@@ -4,18 +4,20 @@ const uuid = require('uuid/v1');
 
 const starling = require('../../banks/starlingAPI');
 
+const vaultConfig = config.get('vault');
 const ouathState = uuid();
 
 router.get('/oauth/login', (req, res) => {
   const starlingConfig = config.get('starling');
-  const clientId = starlingConfig.clientId;
-  const redirectUrl = starlingConfig.redirectUrl;
 
+  const clientId = starlingConfig.clientId;
+  const redirectUrl = `${vaultConfig.apiUrl}/starling/oauth/redirect`;
   const starlingOauthUrl = starlingConfig.oauthUrl;
 
   const url = `${starlingOauthUrl}client_id=${clientId}&response_type=code&state=${ouathState}&redirect_uri=${redirectUrl}`;
 
-  req.session.save(() => {
+  req.session.save((err) => {
+    if (err) console.log(`THERE WAS AN ERROR SAVING THE SESSION: ${err}`);
     res.redirect(url);
   });
 });
@@ -30,12 +32,20 @@ router.get('/oauth/redirect', async (req, res) => {
 
   const accessToken = await starling.getAccessToken(code);
 
-  req.session.reload(() => {
+  req.session.reload((err) => {
+    if (err) console.log(`THERE WAS AN ERROR LOADING THE SESSION: ${err}`);
+
     req.session.accessToken = accessToken.data.access_token;
     req.session.refreshToken = accessToken.data.refresh_token;
 
-    res.redirect('http://localhost:3000/account');
+    res.redirect(`${vaultConfig.clientUrl}/account`);
   });
+});
+
+router.get('/accounts', async (req, res) => {
+  const accounts = await starling.getAccountsList(req.session.accessToken);
+
+  res.send(accounts.data);
 });
 
 module.exports = router;
