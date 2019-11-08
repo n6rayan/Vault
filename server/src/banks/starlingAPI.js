0b09f8b3de03436d8fa5f7e16af0214582d981ac
Nour Rayan <nour@broadbean.com>
2019-11-08 08:32:21 +0000

    Adding function to get accounts list and adding error handling

--- server/src/banks/starlingAPI.js
+++ server/src/banks/starlingAPI.js
@@ -2,25 +2,50 @@ const axios = require('axios');
 const config = require('config');
 const qs = require('query-string');
 
+const log = require('../logger');
+const starlingConfig = config.get('starling');
+const vaultConfig = config.get('vault');
+
 const getAccessToken = async (code) => {
-  const starlingConfig = config.get('starling');
+  const redirectUrl = `${vaultConfig.apiUrl}/starling/oauth/redirect`;
 
   const data = qs.stringify({
     code: code,
     client_id: starlingConfig.clientId,
     client_secret: starlingConfig.clientSecret,
     grant_type: 'authorization_code',
-    redirect_uri: starlingConfig.redirectUrl
+    redirect_uri: redirectUrl
   });
 
-  return await axios({
-    method: 'POST',
-    url: starlingConfig.apiUrl,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: data
-  });
+  try {
+    return await axios({
+      method: 'POST',
+      url: `${starlingConfig.apiUrl}/oauth/access-token`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: data
+    });
+  }
+  catch (err) {
+    log.error(`Problem getting an access token: ${err}`);
+  }
 };
 
+const getAccountsList = async (token) => {
+  try {
+    return await axios({
+      method: 'GET',
+      url: `${starlingConfig.apiUrl}/api/v2/accounts`,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    });
+  }
+  catch (err) {
+    log.error(`Problem retrieving accounts lists: ${err}`);
+  }
+}
+
 module.exports.getAccessToken = getAccessToken;
+module.exports.getAccountsList = getAccountsList;
