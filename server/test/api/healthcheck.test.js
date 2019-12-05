031e3a51f6bf7dad352a5c28dcee022674b8dbc9
Nour Rayan <nour@broadbean.com>
2019-12-05 16:55:43 +0000

    Adding test for healthcheck route

---
+++ server/test/api/healthcheck.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const app = require('../../src/app');
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('Healthcheck Route', function () {
+
+  it ('should GET /healthcheck', async function () {
+    const response = await chai.request(app).get('/api/healthcheck');
+    
+    expect(response).to.have.status(200);
+    expect(response.body.now).to.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/);
+    expect(response.body.status).to.equal('OK');
+  });
+});
