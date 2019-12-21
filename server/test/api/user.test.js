6ad95f1d5fadcd0d4c963d749bec38ffbf65f4b5
Nour Rayan <nour@broadbean.com>
2019-12-21 11:48:16 +0000

    Using Sinon to stub creating of user data

--- server/test/api/user.test.js
+++ server/test/api/user.test.js
@@ -1,7 +1,9 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
 
 const app = require('../../src/app');
+const Database = require('../../src/database');
 
 chai.use(chaiHttp);
 const expect = chai.expect;
@@ -17,9 +19,15 @@ describe('User Routes', function () {
 
   it('should create a user', async function () {
     const userData = require('../fixtures/user.json');
+    sinon.stub(Database.prototype, "createUser").resolves(userData);
+
     const response = await chai.request(app).post('/api/user').send(userData);
 
     expect(response.status).to.equal(200);
     expect(response.body).to.eql({ success: 1, message: 'User created!' });
+  });
+
+  after(function() {
+    sinon.restore();
   });
 });
