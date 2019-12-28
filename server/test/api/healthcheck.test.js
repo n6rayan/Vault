c9608220fe6bfbba7705025e1e66127370d8a244
Nour Rayan <nour@broadbean.com>
2019-12-28 10:42:02 +0000

    Removing sinon stubbing and un-needed Redis mocks

--- server/test/api/healthcheck.test.js
+++ server/test/api/healthcheck.test.js
@@ -1,8 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const sinon = require('sinon');
-const redis = require('redis');
-const mockRedis = require('redis-mock');
 
 const app = require('../../src/app');
 
@@ -11,19 +8,11 @@ const expect = chai.expect;
 
 describe('Healthcheck Route', function () {
 
-  beforeEach(function () {
-    sinon.stub(redis, "createClient").resolves(mockRedis.createClient());
-  });
-
   it ('should GET /healthcheck', async function () {
     const response = await chai.request(app).get('/api/healthcheck');
-    
+
     expect(response).to.have.status(200);
     expect(response.body.now).to.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/);
     expect(response.body.status).to.equal('OK');
-  });
-
-  afterEach(function () {
-    sinon.restore();
   });
 });
