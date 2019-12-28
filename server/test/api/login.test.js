2648297cfeb408718de1ef9ba25b05961d0bf4ef
Nour Rayan <nour@broadbean.com>
2019-12-28 10:43:33 +0000

    Various updates to login test

--- server/test/api/login.test.js
+++ server/test/api/login.test.js
@@ -1,8 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const passport = require('passport');
-const redis = require('redis');
-const mockRedis = require('redis-mock');
 const sinon = require('sinon');
 
 const app = require('../../src/app');
@@ -16,52 +13,49 @@ describe('Login Route - Errors', function () {
   it('should return missing credentials error', async function () {
     const response = await chai.request(app).post('/api/login');
 
-    expect(response.body).to.eql({ message: 'Missing credentials', level: 'info', success: 0 });
+    expect(response.body).to.eql({ error: 'User not found!', success: 0 });
   });
 
   it('should return incorrect credentials error', async function () {
-    const response = await chai.request(app).post('/api/login').send({ username: 'username', password: 'password' });
+    const userData = require('../fixtures/user.json');
+    sinon.stub(helpers, "findUser").resolves(userData);
 
-    expect(response.body).to.eql({ message: 'Incorrect Credentials!', level: 'info', success: 0 });
+    const response = await chai.request(app).post('/api/login').send({ username: 'username', password: 'wrong-password' });
+
+    expect(response.body).to.eql({ error: 'User not found!', success: 0 });
+  });
+
+  after(function() {
+    sinon.restore();
   });
 });
 
 describe('Login/Log Out Routes', function () {
 
-  before(function () {
-    const userData = require('../fixtures/user');
-
-    // sinon.stub(helpers, "findUser").resolves(userData);
-
-    sinon.stub(redis, "createClient").resolves(mockRedis.createClient({}));
-
-    sinon.stub(passport, "authenticate").callsFake((strategy, callback) => {
-      callback(null, { "username": "test@techbrij.com" }, null);
-      return (req, res, next) => { };
-    });
-    sinon.stub(passport, "serializeUser").yields({});
-  });
-
   it('should log user in', async function () {
+    const userData = require('../fixtures/db-user.json');
+    sinon.stub(helpers, "findUser").resolves(userData);
+
     const response = await chai.request(app).post('/api/login').send({ username: 'username', password: 'password' });
 
     expect(response.body.success).to.equal(1);
     expect(response.body.message).to.equal('Successfully logged in!');
   });
 
-  after(function () {
-    sinon.restore();
-  });
+  it('should get current user', async function () {
+    const token = require('../jwtGenerate');
+    const response = await chai.request(app).get('/api/current-user').set('Cookie', `jwt=${token}`);
 
-  // TODO: Test api/current-user route
-  // Add way to generate cookie
-  xit('should get current user', async function () {
-    const response = await chai.request(app).get('/api/current-user');
+    expect(response.body.user.username).to.equal('N6Rayan');
   });
 
   it('should log user out', async function () {
     const response = await chai.request(app).get('/api/logout');
 
     expect(response.body).to.eql({ success: 1, message: 'Successfully logged out!' });
+  });
+
+  after(function() {
+    sinon.restore();
   });
 });
