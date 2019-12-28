491318c64de68b9fc08f57c9f5e2a52cff8769f1
Nour Rayan <nour@broadbean.com>
2019-12-28 11:49:56 +0000

    Adding helper test coverage

---
+++ server/test/helpers/findUser.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const Database = require('../../src/database');
+const findUser = require('../../src/helpers/findUser');
+
+describe('Testing Find User Helper', function () {
+
+  before(function() {
+    const userData = require('../fixtures/db-user');
+    sinon.stub(Database.prototype, "getUserByUsername").resolves(userData);
+  });
+
+  it('should return a user', async function () {
+    const user = await findUser('username');
+
+    chai.expect(user).to.be.an('Object');
+    chai.expect(user.username).to.equal('username');
+    chai.expect(user.name).to.equal('User Name');
+    chai.expect(user.email).to.equal('user.name@email.com');
+  });
+
+  after(function() {
+    sinon.restore();
+  });
+});
--- server/test/helpers/storeTokensAgainstJWT.test.js
+++ server/test/helpers/storeTokensAgainstJWT.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const config = require('config');
 const jwt = require('jsonwebtoken');
 
 const jwtModify = require('../../src/helpers/storingTokensAgainstJWT');
@@ -10,11 +11,11 @@ describe('Testing JWT Modification Helper', function () {
   });
 
   it('should return a JWT and include new modifications', function () {
-    const jsonwebtoken = jwtModify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.rznY5j2yATtnFeFNkykjaVTHb-ej78ZjYOpyi7qE0xA', { access_token: 'accessToken', refresh_token: 'refreshToken' });
+    const jsonwebtoken = jwtModify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.KX8kzUd8mN1BxR3iPZWXc7gKQDMa5GgxlY-Tp-ih89A', { access_token: 'accessToken', refresh_token: 'refreshToken' });
 
     chai.expect(jsonwebtoken).to.match(/eyJ.+\.eyJ.+\..+/);
 
-    const payload = jwt.verify(jsonwebtoken, 'PyXqNfZBfiGy87pw');
+    const payload = jwt.verify(jsonwebtoken, config.get('jwt.secret'));
 
     chai.expect(payload).to.contain({ accessToken: 'accessToken', refreshToken: 'refreshToken' });
   });
