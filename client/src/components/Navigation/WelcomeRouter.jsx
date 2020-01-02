e89814bbe7fc0557d3fd119bcac230d0885d4b2b
Nour Rayan <nour@broadbean.com>
2020-01-02 15:24:07 +0000

    Reverting file back to original

--- client/src/components/Navigation/WelcomeRouter.jsx
+++ client/src/components/Navigation/WelcomeRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 
 import PropTypes from 'prop-types';
-import { Route } from "react-router-dom";
 
 import Welcome from '../Welcome';
 import Login from '../Login';
