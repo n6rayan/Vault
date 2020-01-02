1dba9b3a48590f445dc946b0299d9b9e67ba6e06
Nour Rayan <nour@broadbean.com>
2020-01-02 14:16:11 +0000

    Adding JS Doc to functions

--- server/src/helpers/findUser.js
+++ server/src/helpers/findUser.js
@@ -1,5 +1,10 @@
 const Database = require('../database');
 
+/**
+ * Retrieves the user by the username specified
+ * @param {String} username
+ * @returns {Object} Returns the details of the user
+ */
 const findUser = async (username) => {
   const db = new Database();
 
