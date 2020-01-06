f2e687b71de3309a2ebdcdaf3715709d8d24ff95
Nour Rayan <nour@broadbean.com>
2020-01-06 08:54:52 +0000

    Adding MailJet config and alphabetising

--- server/config/default.js
+++ server/config/default.js
@@ -1,16 +1,22 @@
 module.exports = {
-  vault: {
-    apiUrl: 'http://vault.io:3001/api',
-    clientUrl: 'http://vault.io:3000'
+  crypto: {
+    secret: '6v9y$B&E(H+MbQeT'
+  },
+  jwt: {
+    secret: 'PyXqNfZBfiGy87pw'
+  },
+  mailjet: {
+    apiKey: 'f89a18736bcd1e4432162509e3c56251',
+    fromEmail: 'n6rayan@gmail.com',
+    fromName: 'Vault',
+    secretKey: '48e4266dc0d1fe4f2790df5984f667ea',
   },
   starling: {
     clientId: 'f853knrVYfhuV4ytrKfE',
     clientSecret: '0uMqJeqjzLNaUXD2mMpVygr7jF53akRy6ToyNZCe'
   },
-  jwt: {
-    secret: 'PyXqNfZBfiGy87pw'
-  },
-  crypto: {
-    secret: '6v9y$B&E(H+MbQeT'
+  vault: {
+    apiUrl: 'http://vault.io:3001/api',
+    clientUrl: 'http://vault.io:3000'
   }
 };
