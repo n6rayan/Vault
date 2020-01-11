b08dc02d7f7fc1547cc8139ab9f52d5637673cbe
Nour Rayan <nour@broadbean.com>
2020-01-11 09:48:39 +0000

    Removing failed helper

--- server/src/helpers/hashGenerator.js
+++
@@ -1,7 +0,0 @@
-const config = require('config');
-const crypto = require('crypto');
-
-module.exports = (data) => {
-  return crypto.createHmac('sha256', config.get('crypto.secret'))
-    .update(data).digest('hex');
-};
