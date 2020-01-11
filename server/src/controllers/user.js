60cae38684e250764f9f92ab9471a9418110082b
Nour Rayan <nour@broadbean.com>
2020-01-11 09:47:11 +0000

    Adding loging for confirming a users email address

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,3 +1,5 @@
+const config = require('config');
+
 const Database = require('../database');
 const emailDispatcher = require('../emailDispatcher');
 const helpers = require('../helpers');
@@ -15,19 +17,19 @@ const createUser = async (req, res) => {
 
   try {
     user.password = helpers.hashUserPassword(user.password);
-    user.emailHash = helpers.hashGenerator(user.username);
+    user.emailToken = helpers.jwtGenerate({ username: user.username })
 
     await db.createUser(user);
 
-    const sentEmail = await emailDispatcher.sendConfirmationEmail(user.username, user.email, user.emailHash);
+    const sentEmail = await emailDispatcher.sendConfirmationEmail(user.username, user.email, user.emailToken);
 
-    if (!sentEmail.body.sent.length) {
+    if (!sentEmail.body.Sent.length) {
       throw new Error(`Email address: [${user.email}] is unreachable!`);
     }
 
     return res.status(200).send({
       success: 1,
-      message: "User created!"
+      message: 'User created!'
     });
   }
   catch (err) {
@@ -35,13 +37,36 @@ const createUser = async (req, res) => {
 
     return res.status(400).send({
       success: 0,
-      error: "Unable to create user!"
+      error: 'Unable to create user!'
     });
   }
 }
 
 const confirmUserAccount = async (req, res) => {
-  const emailHash = req.query.data;
+  const emailToken = req.query.data;
+  const vaultConfig = config.get('vault');
+
+  try {
+    const payload = helpers.jwtVerify(emailToken);
+
+    const userData = {
+      isEmailConfirmed: true,
+      emailToken: ''
+    };
+
+    const updateUser = await db.updateUser({ username: payload.username }, userData);
+    console.log(updateUser);
+
+    return res.redirect(vaultConfig.clientUrl);
+  }
+  catch (err) {
+    log.error(err);
+
+    return res.status(400).send({
+      success: 0,
+      message: 'Unable to confirm users email address!'
+    });
+  }
 }
 
 module.exports.createUser = createUser;
