e3a80c4afb1ce119f7d7b0df4858860931565317
Nour Rayan <nour@broadbean.com>
2020-02-07 16:59:56 +0000

    Additonal GET /user route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const Controller = require('../controllers/user');
 
 router.post('/user', async (req, res) => Controller.createUser(req, res));
+router.get('/user/:username', async (req, res) => Controller.getUser(req, res));
 router.get('/user/confirm', async (req, res) => Controller.confirmUserAccount(req, res));
 
 module.exports = router;
